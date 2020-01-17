f2e7adb584b7e2dd94e074510265f58142321b05
Ken <klough@gmx.com>
2020-01-17 14:48:29 -0700

    firat commit

---
+++ converter.java
@@ -0,0 +1,34 @@
+public class Converter
+
+{ 
+//Your names go here:
+
+
+/*
+* @Author: 	Kevin
+				Ken
+				Name of the third student
+*
+*/
+
+private double celsiusToFahrenheit(doubleC){
+	// TODO: The third student will implement this method
+	return0;
+	
+	}
+	
+	private double fahrenheitToCelsius(doubleF){
+		// TODO: The second student will implement this method
+		return0;
+		
+		}
+		
+	public static voidmain(String[] args) {
+		
+		//TODO:The first student will implement this method.
+		//Call CelsiusToFahrenheit to convert 180 Celsiusto Fahrenheitvalue.
+		// Call FahrenheitToCelsius to convert 250 Fahrenheitto Celsiusvalue.
+	
+	}
+	
+	}
