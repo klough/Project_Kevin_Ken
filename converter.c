2904781a0133af5ced4284d8425f48ebc2e4f080
Ken <klough@gmx.com>
2020-01-17 15:35:58 -0700

    New Test

---
+++ converter.c
@@ -0,0 +1,38 @@
+public class Converter
+
+{ 
+//Your names go here:
+
+
+/*
+* @Author: 	Kevin
+				Ken
+				Name of the third student
+*
+*/
+
+private double celsiusToFahrenheit(double C){
+	// TODO: The third student will implement this method
+	
+		return (C*9/5)+32;
+	
+	}
+	
+private double fahrenheitToCelsius(double F){
+		// TODO: The second student will implement this method
+		
+		
+		return (F-32)*5/9;
+		
+	}
+		
+public static voidmain(String[] args) {
+		
+		//TODO:The first student will implement this method.
+		//Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
+		// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
+		String[0] = celsiusToFahrenheit(180);
+		String[1] = fahrenheitToCelsius(250);
+	}
+	
+	}
