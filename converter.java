618a2803a282f7c682f55db159e2c3384d54d052
klough <klough@gmx.com>
2020-01-24 13:21:03 -0700

    Update converter.java

--- converter.java
+++ converter.java
@@ -22,17 +22,19 @@ private double fahrenheitToCelsius(double F){
 		// TODO: The second student will implement this method
 		
 		
-		return (F - 32) * 5/9;
+		return (F-32)*5/9;
 		
 	}
 		
-public static voidmain(String[] args) {
+public static void main(String[] args) {
 		
 		//TODO:The first student will implement this method.
 		//Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
 		// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
-		String[0] = celsiusToFahrenheit(180);
-		String[1] = fahrenheitToCelsius(250);
+		Converter C = new Converter(); 
+		System.out.println(C.celsiusToFahrenheit(180)); //180 System.out.println
+		//C.celsiusToFahrenheit(180);
+		System.out.println(C.fahrenheitToCelsius(250)); //250
 	}
 	
 	}
