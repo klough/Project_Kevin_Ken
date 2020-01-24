203d4d9708c964459ae36b884bc111cee03506d8
Ken <klough@gmx.com>
2020-01-24 12:46:15 -0700

    fixed issues

--- converter.java
+++ converter.java
@@ -31,7 +31,7 @@ public static void main(String[] args) {
 		//TODO:The first student will implement this method.
 		//Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
 		// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
-		C = new Converter; 
+		C = new Converter(); 
 		args[0] = C.celsiusToFahrenheit(180);
 		args[1] = C.fahrenheitToCelsius(250);
 	}
